b68b494959dfc3329992e05de903a870eaaaa95c
Kieron <kierondowie@gmail.com>
2015-12-02 17:18:43 +0200

    x

--- database.js
+++ database.js
@@ -6,29 +6,9 @@ var connection;
 
 module.exports = {
 	connect:function(){
-		
-		/*var connectString = process.env.DATABASE_URL || 'localhost';
+		var connectString = process.env.DATABASE_URL || 'localhost';
 		var client = new pg.Client(connectString);
-		client.connect();*/
-		var db_config = {
-			host: 'ec2-23-23-199-181.compute-1.amazonaws.com',
-			user: 'aubtmwsueljmlo',
-			port: 5432,
-			password: 'NgGgFPZeIadarZo81gp-1EKN93',
-			database: 'd80g2kksssndck'
-		};
-		console.log('Connecting to MySQL database...');
-		connection = mysql.createConnection(db_config);
-		connection.connect(function(err){
-			if (err) 
-			{
-				throw err;
-			}
-			else
-			{
-				console.log('Connected!');
-			}
-		});
+		client.connect();	
 	},
 	query: function(query, callback){
 		connection.query(query,callback);
