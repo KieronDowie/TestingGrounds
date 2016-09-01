51241ee3cc76aa09ddbb3b2cc7f473b74082c3f0
Kieron <kierondowie@gmail.com>
2016-09-01 19:48:33 +0200

    Fixed bug where mod received /t message

--- server.js
+++ server.js
@@ -1368,7 +1368,7 @@ function setPhase(p)
 				}
 			}
 			//Target info, else if because you do not recieve it if you are jailed.
-			else
+			else if (i != mod)
 			{
 				players[i].s.emit(Type.SYSTEM,'Use "/target name" or "/t name" to send in your night action.');
 			}
