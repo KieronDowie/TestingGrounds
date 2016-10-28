47659934c5d1e188510314954dbf4b03ff76055b
PascalGamer <pascalgamer007@gmail.com>
2016-10-28 14:52:48 +0200

    Halloween

--- playscript.js
+++ playscript.js
@@ -67,10 +67,16 @@ function mutemusic(phase)
 	{
 		lobby.volume = 1;
 		lobbyon = 1;
+		if (phase == 0)
+		{
+			lobby.play();
+		}
 	}
 	else
 	{
 		lobby.volume = 0;
+		lobby.pause();
+		lobby.currentTime = 0;
 		lobbyon = 0;
 	}
 }
