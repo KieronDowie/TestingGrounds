58d11c15f05b1d1ad73ba870c064c512be3acc95
PascalGamer <pascalgamer007@gmail.com>
2016-11-17 22:36:59 +0100

    RAIN

--- gm.js
+++ gm.js
@@ -857,11 +857,13 @@ module.exports = {
 				var role = getRole(targets[num]);
 				var roleInfo = autoRoles[role];
 				var roleAttributes = roleInfo.attributes;
+				playersByName[players[i].name] = players[i];//Quicktarget
+				targets[players[i].name] = [players[i].role,undefined, true];
 				if (roleAttributes.NINJA || roleAttributes.NOLIVINGTARGET)
 				{
 					
 				}
-				else
+				else if (targets[players[i].name][1] != [])
 				{
 					drstring += " "+num;
 				}
