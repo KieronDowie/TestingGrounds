e60e7ea6684c9a1d00d3c2bacea9b37975edc6f5
Ralozey <ralozey@gmail.com>
2016-11-29 22:07:38 +0100

    Medium Fix

--- server.js
+++ server.js
@@ -3890,7 +3890,7 @@ function Player(socket,name,ip)
 								this.specMessage(msg,{dead:true},'Medium');
 								//Echo the message back to the medium.
 								this.s.emit(Type.MSG,'Medium',{msg:msg,styling:'dead'});
-								}
+								};
 							}
 							if (this.beingSeanced)
 							{
