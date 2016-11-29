93c412abf537307f6aa9bce4baf1e9bd7e8e5c0e
Ralozey <ralozey@gmail.com>
2016-11-29 23:46:58 +0100

    Medium Fix

--- server.js
+++ server.js
@@ -2167,17 +2167,7 @@ function Player(socket,name,ip)
 												}
 												else if (mod == target.s.id)
 												{
-													if (medium.seancing && medium.seancing == target)
-												{
-													medium.s.emit(Type.SYSTEM, 'You cancel your seance.');
-													medium.seancing.beingSeanced = undefined;
-													medium.seancing = undefined;
-													players[mod].s.emit(Type.SYSTEM,medium.name+' cancels their seance.');
-												}
-												else
-												{
-													medium.s.emit(Type.SYSTEM, 'You are not targetting anyone.');
-												}
+													medium.s.emit(Type.SYSTEM,'You cannot seance the MOD.');
 												}
 												else if (medium.seancing && medium.seancing == target)
 												{
