e907622d85f3b27e2f7c169b82897c7c9a03a17c
Ralozey <ralozey@gmail.com>
2016-12-01 09:31:50 +0100

    Doctor-BG-SK Fix

--- gm.js
+++ gm.js
@@ -1162,14 +1162,17 @@ module.exports = {
 											var name = visitors[j];
 											var role = getRole(targets[name]);
 											var autorole = autoRoles[role];
-											var roleofbgtarget = autoRoles[visitorsofbg];
+											for (r in visitorsofbg)
+											{
+											var rolebg = getRole(targets[visitorsofbg[r]]);
+											var roleofbgtarget = autoRoles[rolebg];
 											if (autorole !== undefined)
 											{
 												var attrib = autorole.attributes;
 												if (attrib.MAFKILL || attrib.VIGKILL || attrib.DOUSE || attrib.MAUL || attrib.SKKILL)
 												{
 													addSuggestedMessage("Someone tried to attack you, but someone fought off your attacker." ,t[0]); //Tell target they were guarded.
-													addSuggestedMessage('You were killed by a bodyguard.'+roleofbgtarget,visitors[j]); //Tell attacker they were killed by a bg
+													addSuggestedMessage('You were killed by a bodyguard.'+rroleofbgtarget,visitors[j]); //Tell attacker they were killed by a bg
 													addSuggestedMessage('You died protecting your target.',num); //Tell bg that they succeeded
 													//Suggested actions: Kill both the bg and attacker
 													addSuggestedAction('Kill', num);
@@ -1181,6 +1184,7 @@ module.exports = {
 													targets[num].bgKill = visitors[j];
 													break; //break, bg can only stop one attacker.
 												}
+											}
 											}
 										}
 									}
