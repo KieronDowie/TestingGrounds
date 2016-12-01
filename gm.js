ebcb8d70cf10d9641c1ff649302dc783d2534888
Ralozey <ralozey@gmail.com>
2016-12-01 12:54:53 +0100

    Doctor-BG-SK Fix

--- gm.js
+++ gm.js
@@ -1176,16 +1176,16 @@ module.exports = {
 													if (attribbg.HEAL)
 													{
 													addSuggestedMessage('You successfully protected your target and someone nursed you back to health!',num); //Tell bg that they succeeded but got healed
-													addSuggestedMessage('Your target was attacked last night.',[visitorsofbg[r]);
+													addSuggestedMessage('Your target was attacked last night.',[visitorsofbg[r]]);
 													successfulheal = true;
 													}
-													}/*
+													}
 													if (!successfulheal)
 													{
 													addSuggestedMessage('You died protecting your target.',num); //Tell bg that they succeeded
 													addSuggestedAction('Kill', num);
 													addSuggestedMessage('They died guarding someone.','<All>');
-													}*/
+													}
 													addSuggestedMessage("Someone tried to attack you, but someone fought off your attacker." ,t[0]); //Tell target they were guarded.
 													addSuggestedMessage('You were killed by a bodyguard.',visitors[j]); //Tell attacker they were killed by a bg
 													//Suggested actions: Kill both the bg and attacker
