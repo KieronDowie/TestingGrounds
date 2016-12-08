9d635384ab747adfaf3738883a3b4834a1ac3fde
Ralozey <ralozey@gmail.com>
2016-12-08 23:47:01 +0100

    Spectator Stuff

--- server.js
+++ server.js
@@ -1496,7 +1496,7 @@ function numLivingPlayers()
 	var c = 0;
 	for (i in players)
 	{
-		if (players[i].alive && i!=mod && i!=spectate)
+		if (players[i].alive && i!=mod && !players[i].spectate)
 		{
 			c++;
 		}
