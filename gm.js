d6422f16f454aaac8e3b21e7be7522b3a2562d00
Ralozey <ralozey@gmail.com>
2017-01-23 17:01:14 +0100

    Arsonist Automated

--- gm.js
+++ gm.js
@@ -1236,15 +1236,6 @@ module.exports = {
 									    var t = targets[num][1];
 									    var peopleTargetting = getPeopleTargetting(t[0]);
 									    var attackSuccess = true;
-									    var selfVisiting = false;
-									    for (i in arr) {
-									        if (!isNaN(arr[i])) {
-									            arr[i] = players[playernums[arr[i]]].name;
-									        }
-									        if (arr[i] == self.name) {
-									            selfVisiting = true;
-									        }
-									    }
 									    if (autoRoles[role] && autoRoles[role].attributes.ALERT) //Vet alert.
 									    {
 									        if (Object.keys(targets[t[0]][1]).length != 0) //If alerting
@@ -1255,7 +1246,7 @@ module.exports = {
 									    }
 									    if (attackSuccess) {
 									        addSuggestedAction('Douse', t[0]);
-									        addSuggestedMessage(`t[0] = ${t[0]}`, t[0]);
+									        addSuggestedMessage(`t[0] = ${t[0]} num = ${num}`, t[0]);
 									    }
 									}
 									else if (roleAttributes.MAFKILL || roleAttributes.SKKILL || roleAttributes.VIGKILL) {
